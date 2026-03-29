87cbd1e012e29a6ddd1561e7a1ed066ce4447499
JhoneeM <jhoneemiles@gmail.com>
2026-03-29 03:11:37 -0400

    Fix import name for cookie-parser module

--- index.js.js
+++ index.js.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cookieparser from "cookie-parser";
+import cookieParser from "cookie-parser";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
