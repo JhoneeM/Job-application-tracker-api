08095146f02134b2410146fc6855c9c5dbca7401
JhoneeM <jhoneemiles@gmail.com>
2026-03-29 02:57:30 -0400

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieparser from "cookie-parser";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/",(req,res) => {
+ res.send("Job Application Tracker API is running");
+});
+
+app.get("/health", (req,res) => {
+ res.json({status: "ok"});
+});
+
+app.listen(PORT, () => {
+ console.log(`Server running on port ${PORT}`);
